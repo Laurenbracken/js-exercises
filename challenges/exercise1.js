e71342f0e5847d63852749c7dee09da5d651d190
Lauren Bracken <laurenbracken@Alexeis-MacBook-Air.local>
2024-01-22 17:16:44 +0000

    Implemented first exercise

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,21 +1,28 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
 	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
+	return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
 	// Your code here!
+	const peoplePerBus = 40;
+	const numberOfBuses = Math.ceil(people / peoplePerBus)
+	return numberOfBuses;
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
 	// Your code here!
+	const sheepCount = arr.filter(animal => animal.toLowerCase() === 'sheep').length;
+	return sheepCount;
 }
 
 export function hasMPostCode(person) {
